1b5a6c2672184d567250d976dda61ee158826ed7
devopsuddin <devopsuddin@gmail.com>
2020-06-02 07:14:25 +0530

    add initial app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Hello World!\n");git config --global user.name
+console.log("Ayaan Ahammad!\n")
+console.log("Ashmaan Ahammad!\n")
+console.log("Aafiah Fathima!\n")
+console.log("Aizah Fathima!\n")
+
